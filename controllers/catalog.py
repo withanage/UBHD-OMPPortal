3f96b9ab9804f3f08b637f64e4a987668bc5dd71
withanage <dulip.withanage@gmail.com>
2015-10-02 12:55:59 +0200

    [info] code optmize

--- controllers/catalog.py
+++ controllers/catalog.py
@@ -35,7 +35,7 @@ def index():
     return dict(submissions=submissions, subs=subs, authors=authors)
 
 def book():
-  abstract, authors, cleanTitle, subtitle  = '', '', '', ''
+  abstract, authors, cleanTitle, press_name, subtitle  = '', '', '', '', ''
   locale = 'de_DE'
   if session.forced_language=='en':
    locale= 'en_US'
@@ -57,7 +57,12 @@ def book():
   chapter_query = ((db.submission_chapters.submission_id==book_id) & (db.submission_chapters.chapter_id ==db.submission_chapter_settings.chapter_id ) & (db.submission_chapter_settings.locale==locale) & (db.submission_file_settings.setting_name=="chapterID") &(db.submission_file_settings.setting_value==db.submission_chapters.chapter_id) & (db.submission_file_settings.file_id==db.submission_files.file_id) & (db.submission_chapter_settings.setting_name=='title') ) 
   chapters = db(chapter_query).select(db.submission_chapters.chapter_id, db.submission_chapter_settings.setting_value,db.submission_files.assoc_id, db.submission_files.submission_id, db.submission_files.genre_id, db.submission_files.file_id, db.submission_files.revision, db.submission_files.file_stage, db.submission_files.date_uploaded, orderby=[db.submission_chapters.chapter_id,db.submission_files.assoc_id])
   publication_formats= db((db.publication_formats.submission_id==book_id) & (db.publication_format_settings.publication_format_id==db.publication_formats.publication_format_id) & (db.publication_format_settings.locale==locale)).select(db.publication_format_settings.setting_name,db.publication_format_settings.setting_value,db.publication_formats.publication_format_id, groupby=db.publication_formats.publication_format_id, orderby=db.publication_formats.publication_format_id)
-  
+  press_settings = db(db.press_settings.press_id==myconf.take('omp.press_id') & (db.press_settings.locale==locale)).select(db.press_settings.setting_name, db.press_settings.setting_value)
+
+  for i in press_settings:
+    if i.setting_name=='name':
+      press_name = i.setting_value
+
   for  i in book:
     if i.setting_name=='abstract':
       abstract = i.setting_value
@@ -66,5 +71,5 @@ def book():
     if i.setting_name =='cleanTitle':
       cleanTitle =  i.setting_value
   cover_image = URL(myconf.take('web.application'),'static','monographs/'+ book_id+'/simple/cover.jpg')
-  return dict(abstract=abstract,authors=authors, author_bio=author_bio, chapters=chapters,  cleanTitle=cleanTitle,cover_image=cover_image,publication_formats=publication_formats,  subtitle=subtitle)
+  return dict(abstract=abstract,authors=authors, author_bio=author_bio, chapters=chapters,  cleanTitle=cleanTitle,cover_image=cover_image,publication_formats=publication_formats,  subtitle=subtitle, press_name=press_name)
 
