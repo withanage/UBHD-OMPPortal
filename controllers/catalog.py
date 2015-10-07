728ede67940f3df9b24dd00fe56b33577e2bfebc
withanage <dulip.withanage@gmail.com>
2015-10-07 11:21:58 +0200

    [info] documentatoin

--- controllers/catalog.py
+++ controllers/catalog.py
@@ -36,30 +36,45 @@ def book():
   if session.forced_language=='en':
    locale= 'en_US'
   book_id =  request.args[0] if request.args else redirect(URL('home','index')) 
+  
   query =  ((db.submission_settings.submission_id == int(book_id)) & (db.submission_settings.locale==locale))
   book = db(query).select(db.submission_settings.ALL)
+  
   if len(book) ==0:
      raise HTTP(404,T('book not found'))
+  
   author_q = ((db.authors.submission_id==book_id)) 
   authors_list = db(author_q).select(db.authors.first_name,db.authors.last_name)
+  
   for i in authors_list:
     authors += i.first_name +' '+ i.last_name+', '
   if authors.endswith(', '):
     authors = authors[:-2]
+  
   author_bio = db((db.authors.submission_id==book_id) & (db.authors.author_id == db.author_settings.author_id) & (db.author_settings.locale==locale) & ( db.author_settings.setting_name=='biography')).select(db.author_settings.setting_value).first()
+  
   chapter_query = ((db.submission_chapters.submission_id==book_id) & (db.submission_chapters.chapter_id ==db.submission_chapter_settings.chapter_id ) & (db.submission_chapter_settings.locale==locale) & (db.submission_file_settings.setting_name=="chapterID") &(db.submission_file_settings.setting_value==db.submission_chapters.chapter_id) & (db.submission_file_settings.file_id==db.submission_files.file_id) & (db.submission_chapter_settings.setting_name=='title') ) 
+  
   chapters = db(chapter_query).select(db.submission_chapters.chapter_id, db.submission_chapter_settings.setting_value,db.submission_files.assoc_id, db.submission_files.submission_id, db.submission_files.genre_id, db.submission_files.file_id, db.submission_files.revision, db.submission_files.file_stage, db.submission_files.date_uploaded, orderby=[db.submission_chapters.chapter_id,db.submission_files.assoc_id])
+  
   pub_query = (db.publication_formats.submission_id==book_id) & (db.publication_format_settings.publication_format_id==db.publication_formats.publication_format_id) & (db.publication_format_settings.locale==locale)  & (db.publication_format_settings.setting_value != myconf.take('omp.ignore_format'))
+  
   publication_formats= db(pub_query).select(db.publication_format_settings.setting_name,db.publication_format_settings.setting_value,db.publication_formats.publication_format_id, groupby=db.publication_formats.publication_format_id, orderby=db.publication_formats.publication_format_id)
+  
   press_settings = db(db.press_settings.press_id==myconf.take('omp.press_id')).select(db.press_settings.setting_name, db.press_settings.setting_value)
+  
   publication_format_settings = db((db.publication_format_settings.setting_name=='name')  & (db.publication_format_settings.locale==locale) & (db.publication_formats.submission_id== book_id) & (db.publication_formats.publication_format_id == db.publication_format_settings.publication_format_id)).select(db.publication_format_settings.publication_format_id, db.publication_format_settings.setting_value)
+  
   if publication_format_settings:
     publication_format_settings_doi = db((db.publication_format_settings.setting_name=='pub-id::doi') & (db.publication_format_settings.publication_format_id == publication_format_settings.first()['publication_format_id']) &  (publication_format_settings.first()['setting_value']==myconf.take('omp.doi_format_name'))).select(db.publication_format_settings.setting_value).first()
   identification_codes = db(pub_query & (db.publication_format_settings.publication_format_id==db.identification_codes.publication_format_id)).select(db.identification_codes.value, db.publication_format_settings.setting_value)
+  
   published_date = db(pub_query & (db.publication_format_settings.setting_value== myconf.take('omp.doi_format_name')) & (db.publication_dates.publication_format_id == db.publication_format_settings.publication_format_id )).select(db.publication_dates.date)
+  
   for j in press_settings:
     if j.setting_name=='name':
         press_name = j.setting_value
+  
   for  i in book:
     if i.setting_name=='abstract':
       abstract = i.setting_value
@@ -67,5 +82,7 @@ def book():
       subtitle = i.setting_value
     if i.setting_name =='cleanTitle':
       cleanTitle =  i.setting_value
+  
   cover_image = URL(myconf.take('web.application'),'static','monographs/'+ book_id+'/simple/cover.jpg')
-  return dict( abstract=abstract,authors=authors, author_bio=author_bio, chapters=chapters,  cleanTitle=cleanTitle,cover_image=cover_image, identification_codes=identification_codes, publication_formats=publication_formats,  publication_format_settings_doi=publication_format_settings_doi, published_date=published_date,  subtitle=subtitle, press_name=press_name)
+  
+return dict( abstract=abstract,authors=authors, author_bio=author_bio, chapters=chapters,  cleanTitle=cleanTitle,cover_image=cover_image, identification_codes=identification_codes, publication_formats=publication_formats,  publication_format_settings_doi=publication_format_settings_doi, published_date=published_date,  subtitle=subtitle, press_name=press_name)
