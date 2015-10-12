908fb31d4950473bc8a89857da3b9a888a4d11a3
withanage <dulip.withanage@gmail.com>
2015-10-12 15:02:24 +0200

    [info] doc update

--- controllers/catalog.py
+++ controllers/catalog.py
@@ -72,8 +72,8 @@ def book():
     pub_query = (db.publication_formats.submission_id == book_id) & (db.publication_format_settings.publication_format_id == db.publication_formats.publication_format_id) & (
         db.publication_format_settings.locale == locale)
 
-    publication_formats = db(pub_query  & (db.publication_format_settings.setting_value != myconf.take('omp.ignore_format'))).select(db.publication_format_settings.setting_name, db.publication_format_settings.setting_value,
-                                               db.publication_formats.publication_format_id, groupby=db.publication_formats.publication_format_id, orderby=db.publication_formats.publication_format_id)
+    publication_formats = db(pub_query & (db.publication_format_settings.setting_value != myconf.take('omp.ignore_format'))).select(db.publication_format_settings.setting_name, db.publication_format_settings.setting_value,
+                                                                                                                                    db.publication_formats.publication_format_id, groupby=db.publication_formats.publication_format_id, orderby=db.publication_formats.publication_format_id)
 
     press_settings = db(db.press_settings.press_id == myconf.take('omp.press_id')).select(
         db.press_settings.setting_name, db.press_settings.setting_value)
@@ -84,22 +84,37 @@ def book():
     if publication_format_settings:
         publication_format_settings_doi = db((db.publication_format_settings.setting_name == 'pub-id::doi') & (db.publication_format_settings.publication_format_id == publication_format_settings.first(
         )['publication_format_id']) & (publication_format_settings.first()['setting_value'] == myconf.take('omp.doi_format_name'))).select(db.publication_format_settings.setting_value).first()
-    
-    identification_codes={}
-    identification_codes_publication_formats = db(db.publication_formats.submission_id==book_id   ).select(db.publication_formats.publication_format_id)
-    
-    for i in identification_codes_publication_formats: 
-      name = db( (db.publication_format_settings.locale == locale) & (db.publication_format_settings.publication_format_id==i['publication_format_id']) & (db.publication_format_settings.setting_name=='name' )  &  (db.publication_format_settings.setting_value != myconf.take('omp.xml_category_name'))) .select (db.publication_format_settings.setting_value).first()
-      identification_code = db((db.identification_codes.publication_format_id==i['publication_format_id'])  & (db.identification_codes.code ==15)).select (db.identification_codes.value).first()
-      if name and identification_code:
-        identification_codes[identification_code['value']]=name['setting_value']
-        
+
+    identification_codes = {}
+    identification_codes_publication_formats = db(
+        db.publication_formats.submission_id == book_id).select(
+        db.publication_formats.publication_format_id)
+
+    for i in identification_codes_publication_formats:
+        name = db(
+            (db.publication_format_settings.locale == locale) & (
+                db.publication_format_settings.publication_format_id == i['publication_format_id']) & (
+                db.publication_format_settings.setting_name == 'name') & (
+                db.publication_format_settings.setting_value != myconf.take('omp.xml_category_name'))) .select(
+                    db.publication_format_settings.setting_value).first()
+        identification_code = db(
+            (db.identification_codes.publication_format_id == i['publication_format_id']) & (
+                db.identification_codes.code == 15)).select(
+            db.identification_codes.value).first()
+        if name and identification_code:
+            identification_codes[
+                identification_code['value']] = name['setting_value']
 
     published_date = db(pub_query & (db.publication_format_settings.setting_value == myconf.take('omp.doi_format_name')) & (
         db.publication_dates.publication_format_id == db.publication_format_settings.publication_format_id)).select(db.publication_dates.date)
-    
-    representatives = db((db.representatives.submission_id==book_id) & (db.representatives.representative_id_type==myconf.take('omp.representative_id_type'))).select(db.representatives.name , db.representatives.url, orderby=db.representatives.representative_id)
-    
+
+    representatives = db(
+        (db.representatives.submission_id == book_id) & (
+            db.representatives.representative_id_type == myconf.take('omp.representative_id_type'))).select(
+        db.representatives.name,
+        db.representatives.url,
+        orderby=db.representatives.representative_id)
+
     full_files = db((db.submission_files.submission_id == book_id) & (db.submission_files.genre_id == myconf.take('omp.monograph_type_id'))).select(db.submission_files.original_file_name, db.submission_files.submission_id, db.submission_files.genre_id,
                                                                                                                                                     db.submission_files.file_id, db.submission_files.revision, db.submission_files.file_stage, db.submission_files.date_uploaded)
 
