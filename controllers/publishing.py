5093528e4b9ebc4c031f0bdcac0510be38eb7a8c
Dulip Withanage <dulip.withanage@gmail.com>
2015-10-15 16:39:49 +0200

    info

---
+++ controllers/publishing.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+Copyright (c) 2015 Heidelberg University Library
+Distributed under the GNU GPL v3. For full terms see the file
+LICENSE.md
+'''
+
+
+def for_authors():
+    return dict()
+
+
+def book_proposal():
+    return dict()
+
+
+def rights_and_licences():
+    return dict()
---
+++ languages/plural-en.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+{
+    # "singular form (0)": ["first plural form (1)", "second plural form (2)", ...],
+    'account': ['accounts'],
+    'book': ['books'],
+    'is': ['are'],
+    'man': ['men'],
+    'miss': ['misses'],
+    'Onix Additionals': ['Onix Additionals'],
+    'person': ['people'],
+    'quark': ['quarks'],
+    'shop': ['shops'],
+    'this': ['these'],
+    'was': ['were'],
+    'woman': ['women'],
+}
