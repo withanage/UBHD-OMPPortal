59fc39f6d1b40a9d6e27c13c6cba04929ecfff72
Dulip Withanage <dulip.withanage@gmail.com>
2015-10-22 11:37:54 +0200

    minor fixes

--- controllers/catalog.py
+++ controllers/catalog.py
@@ -118,8 +118,8 @@ def book():
         db.representatives.url,
         orderby=db.representatives.representative_id)
 
-    #full_files = db((db.submission_files.submission_id == book_id) & (db.submission_files.genre_id == myconf.take('omp.monograph_type_id'))).select(db.submission_files.original_file_name, db.submission_files.submission_id, db.submission_files.genre_id,
-    full_files = db((db.submission_files.submission_id == book_id) & (db.submission_files.file_stage >= 5)).select(db.submission_files.original_file_name, db.submission_files.submission_id, db.submission_files.genre_id,
+    full_files = db((db.submission_files.submission_id == book_id) & (db.submission_files.genre_id == myconf.take('omp.monograph_type_id'))).select(db.submission_files.original_file_name, db.submission_files.submission_id, db.submission_files.genre_id,
+    #full_files = db((db.submission_files.submission_id == book_id) & (db.submission_files.file_stage >= 5)).select(db.submission_files.original_file_name, db.submission_files.submission_id, db.submission_files.genre_id,
                                                                                                                                                     db.submission_files.file_id, db.submission_files.revision, db.submission_files.file_stage, db.submission_files.date_uploaded)
 
     for j in press_settings:
