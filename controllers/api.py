7cbd9cb6f5f3464a43d440e07869f73cd0e2a303
withanage <dulip.withanage@gmail.com>
2015-11-02 14:21:42 +0100

    json  api extension

--- controllers/api.py
+++ controllers/api.py
@@ -28,6 +28,7 @@ def oastatistik():
 
       if book_id.setting_name == 'title':
         title = book_id.setting_value
+      
       authors = {}
       authors_list = db((db.authors.submission_id == book_id.submission_id)).select( db.authors.first_name, db.authors.last_name)
       for i in authors_list:
@@ -52,34 +53,36 @@ def oastatistik():
 
     
       # chapters
-    
-      chapters = db (db.submission_chapters.submission_id == book_id['submission_id']).select(db.submission_chapters.chapter_id, orderby = db.submission_chapters.chapter_seq)
+      
+      
+      chapters = db ((db.submission_chapters.submission_id == book_id['submission_id']) & (db.submission_file_settings.setting_name=='chapterID') & (db.submission_file_settings.setting_value==db.submission_chapters.chapter_id)).select(db.submission_chapters.chapter_id,db.submission_file_settings.file_id, orderby = db.submission_chapters.chapter_seq)
       for c in chapters:
-        chapter_id = str(book_id['submission_id'])+'-'+str(c['chapter_id'])
-        part_filter = False
-        if request.vars.ids:
-          for j in filter_ids:
-            if str(chapter_id) == str(j):
-              part_filter = True
-        if  part_filter or request.vars.ids == None:
-          subs[chapter_id]= {}
-          part_title = db((db.submission_chapter_settings.chapter_id ==  int(c['chapter_id'])) & (db.submission_chapter_settings.locale == locale) & (db.submission_chapter_settings.setting_name == 'title')).select(db.submission_chapter_settings.setting_value).first()
-          if part_title:
-            subs[chapter_id]["label"] = part_title['setting_value']
-          subs[chapter_id]["norm_id"] =  myconf.take('oastatistik.id')+':'+chapter_id
-          subs[chapter_id]["type"] = "part"
-          part_authors = []
-          author_id_list = db(db.submission_chapter_authors.chapter_id ==int(c['chapter_id']) ).select (db.submission_chapter_authors.author_id, orderby = db.submission_chapter_authors.seq)
-          for author in author_id_list :
-            part_authors.append({"type":"person"})
-            part_authors.append({"relation":"creators"})
-            author_name = db((db.authors.author_id == author['author_id'])).select( db.authors.first_name, db.authors.last_name).first()
-            if author_name:
-              part_authors.append({'name': author_name['first_name'] + " "+ author_name['last_name']})
+          chapter_id = str(book_id['submission_id'])+'-'+str(c['submission_chapters']['chapter_id'])
+          file_id = str(book_id['submission_id'])+'-'+str(c['submission_file_settings']['file_id'])
+          part_filter = False
+          if request.vars.ids:
+            for j in filter_ids:
+              if str(file_id) == str(j):
+                part_filter = True
+          if  part_filter or request.vars.ids == None:
+            subs[file_id]= {}
+            part_title = db((db.submission_chapter_settings.chapter_id ==  int(c['submission_chapters']['chapter_id'])) & (db.submission_chapter_settings.locale == locale) & (db.submission_chapter_settings.setting_name == 'title')).select(db.submission_chapter_settings.setting_value).first()
+            if part_title:
+              subs[file_id]["label"] = part_title['setting_value']
+            subs[file_id]["norm_id"] =  myconf.take('oastatistik.id')+':'+chapter_id
+            subs[file_id]["type"] = "part"
+            part_authors = []
+            author_id_list = db(db.submission_chapter_authors.chapter_id ==int(c['submission_chapters']['chapter_id']) ).select (db.submission_chapter_authors.author_id, orderby = db.submission_chapter_authors.seq)
+            for author in author_id_list :
+              part_authors.append({"type":"person"})
+              part_authors.append({"relation":"creators"})
+              author_name = db((db.authors.author_id == author['author_id'])).select( db.authors.first_name, db.authors.last_name).first()
+              if author_name:
+                part_authors.append({'name': author_name['first_name'] + " "+ author_name['last_name']})
 
 
-          subs[chapter_id]["associate_via_hierarchy"] = part_authors
-          subs[chapter_id]["associate_via_hierarchy"] = fullbook
+            subs[file_id]["associate_via_hierarchy"] = part_authors
+            subs[file_id]["associate_via_hierarchy"] = fullbook
     
   
   return sj.dumps(subs, separators=(',', ':'), sort_keys=True)
