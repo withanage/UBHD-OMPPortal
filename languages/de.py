eefe6dfcd74d1985feb6a2f4c25fd87e3a93c127
wwwubhd <wwwredaktion@ub.uni-heidelberg.de>
2015-11-24 13:51:00 +0100

    Medien Forum

--- languages/de.py
+++ languages/de.py
@@ -210,6 +210,7 @@
 'Manage %(action)s': '%(action)s verwalten',
 'Manage Access Control': 'Zugangskontrolle verwalten',
 'Manage Cache': 'Cache verwalten',
+'Mediaforum': 'Mediaforum',
 'Medienbeilage': 'Medienbeilage',
 'Medienbeilage z.B "mit CD-ROM"': 'Medienbeilage z.B "mit CD-ROM"',
 'Mehr': 'Mehr',
