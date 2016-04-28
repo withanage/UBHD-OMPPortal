cd2f70ec37c347080bc6e0db767711b2dd627ba8
wwwubhd <wwwredaktion@ub.uni-heidelberg.de>
2016-04-28 15:49:09 +0200

    [WIT] Chapter ordering in heip, ignore after  DAL intergration

--- controllers/catalog.py
+++ controllers/catalog.py
@@ -92,7 +92,7 @@ def book():
                                                                                                                                                                                                                  "chapterID") & (db.submission_file_settings.setting_value == db.submission_chapters.chapter_id) & (db.submission_file_settings.file_id == db.submission_files.file_id) & (db.submission_chapter_settings.setting_name == 'title'))
 
     chapters = db(chapter_query).select(db.submission_chapters.chapter_id, db.submission_chapter_settings.setting_value, db.submission_files.assoc_id, db.submission_files.submission_id, db.submission_files.genre_id,
-                                        db.submission_files.file_id, db.submission_files.revision, db.submission_files.file_stage, db.submission_files.date_uploaded, orderby=[db.submission_chapters.chapter_id, db.submission_files.assoc_id])
+                                        db.submission_files.file_id, db.submission_files.revision, db.submission_files.file_stage, db.submission_files.date_uploaded, orderby=[db.submission_chapters.chapter_seq, db.submission_chapters.chapter_id, db.submission_files.assoc_id])
 
     pub_query = (db.publication_formats.submission_id == book_id) & (db.publication_format_settings.publication_format_id == db.publication_formats.publication_format_id) & (
         db.publication_format_settings.locale == locale)
