0ab3ca84525322c4c58dee951a93d078c8f50a99
withanage <dulip.withanage@gmail.com>
2017-02-01 17:06:33 +0100

    dynamic menu

--- models/z_menu.py
+++ models/z_menu.py
@@ -4,8 +4,17 @@ Copyright (c) 2015 Heidelberg University Library
 Distributed under the GNU GPL v3. For full terms see the file
 LICENSE.md
 '''
+from ompdal import OMPDAL
+ompdal = OMPDAL(db, myconf)
+
+
 response.title = settings.title
 response.subtitle = settings.subtitle
 response.meta.keywords = settings.keywords
 response.meta.description = settings.description
-response.menu = []
+#response.menu = []
+
+
+categories = ompdal.getCategoriesByPress(myconf.take('omp.press_id'))
+
+response.menu =    [(c['path'], False, URL('default', 'index'), [])  for c in categories.as_list()]
