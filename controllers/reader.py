352e626c85bb6dedacb816ea4250957f9d410ceb
Dulip Withanage <dulip.withanage@gmail.com>
2018-01-09 16:43:56 +0100

    https://gitlab.ub.uni-heidelberg.de/wit/verlag-portale/issues/125

--- controllers/reader.py
+++ controllers/reader.py
@@ -7,50 +7,62 @@ LICENSE.md
 
 # required - do no delete
 import os
+from ompdal import OMPDAL, OMPSettings, OMPItem
+from os.path import exists
+
 
 def user(): return dict(form=auth())
+
+
 # ef download(): return response.download(request,db)
 
 def call():
     session.forget()
     return service()
+
+
 # end requires
 
 def index():
     from gluon.serializers import json
+    ompdal = OMPDAL(db, myconf)
     json_list = dict(xml_url='')
-    locale = 'de_DE'
-    if session.forced_language == 'en':
-        locale = 'en_US'
+
+    locale = 'en_US' if session.forced_language == 'en' else 'de_DE'
+
     submission_id = request.args[0]
     file_id = request.args[1]
 
-    # check if it is xml
     if str(file_id).endswith('.xml'):
-        query = ((db.submission_settings.submission_id == int(submission_id))
-                 & (db.submission_settings.locale == locale))
-        author_q = ((db.authors.submission_id == submission_id))
-        authors_list = db(author_q).select(
+        submission_settings = ompdal.getSubmissionSettings(submission_id)
+        fs = [i for i in submission_settings.as_list() if (i['locale']==locale and i['setting_name']=='fontFamily')]
+        font_family = fs[0]['setting_value'] if len(fs) >0 else 'Source Sans Pro'
+
+        authors_list = db((db.authors.submission_id == submission_id)).select(
             db.authors.first_name, db.authors.last_name)
         authors = ''
         for i in authors_list:
             authors += i.first_name + ' ' + i.last_name + ', '
         if authors.endswith(', '):
             authors = authors[:-2]
-        return dict(json_list=XML(json(json_list)), authors=authors)
+
+        return dict(json_list=XML(json(json_list)), authors=authors, font_family=font_family)
+
     else:
         path = os.path.join(request.folder, 'static/files/presses', myconf.take('omp.press_id'), 'monographs',
                             submission_id, 'submission', file_id)
-       
-        return response.stream(path)
+    return response.stream(path)
+
 
 def home():
     return dict()
+
 
 def index2():
     from gluon.serializers import json
     json_list = dict(xml_url='')
     return dict(json_list=XML(json(json_list)))
+
 
 def download():
     submission_id = request.args[0]
@@ -61,6 +73,7 @@ def download():
     response.headers[
         'Content-Disposition'] = "attachment; filename=" + submission_file
     return response.stream(path, chunk_size=4096)
+
 
 def download_image():
     submission_id = request.args[0]
