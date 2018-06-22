14d2e7b21bfc70ac04af97636b3c66b923ead728
Dulip Withanage <dulip.withanage@gmail.com>
2018-06-22 13:53:15 +0200

    https://gitlab.ub.uni-heidelberg.de/wit/verlag-portale/issues/179

---
+++ controllers/search.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from ompdal import OMPDAL, OMPSettings, OMPItem
+
+
+ompdal = OMPDAL(db, myconf)
+press = ompdal.getPress(myconf.take('omp.press_id'))
+
+def authors():
+    searchInitial = request.vars.searchInitial
+    authors = ompdal.getAuthorsByPress(press.press_id).as_list()
+    index = sorted(set([a['last_name'][:1].upper() for a in authors]))
+
+
+    if searchInitial:
+        authors = [a for a in authors  if a['last_name'].startswith(searchInitial)]
+
+
+    return locals()
+
+
