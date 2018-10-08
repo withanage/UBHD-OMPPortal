09a5f8e783d20b4f61df8a77c43d04677321fde4
Dulip Withanage <dulip.withanage@gmail.com>
2018-10-08 16:57:36 +0200

    https://gitlab.ub.uni-heidelberg.de/wit/verlag-portale/issues/179

--- controllers/search.py
+++ controllers/search.py
@@ -2,20 +2,29 @@
 
 from ompdal import OMPDAL, OMPSettings, OMPItem
 
-
 ompdal = OMPDAL(db, myconf)
 press = ompdal.getPress(myconf.take('omp.press_id'))
 
+
 def authors():
     searchInitial = request.vars.searchInitial
-    authors = ompdal.getAuthorsByPress(press.press_id).as_list()
-    index = sorted(set([a['last_name'] for a in authors]))
+    authors, author_index = [], set()
+    prev_author, prev_index = '',''
+    prev = 1
 
+    for i,a in enumerate(ompdal.getAuthorsByPress(press.press_id).as_list()):
+        initial = a['last_name'].decode('utf-8')[:1]
+        if initial != prev_index:
+            a['initial'] = initial.upper()
+        this_author = '{}{}'.format(a['last_name'], a['first_name'])
+        prev = prev + 1 if prev_author == this_author else 1
+        a['index'] = prev
+        authors.append(a)
+        author_index.add(initial)
+        prev_author = this_author
+        prev_index = initial
 
     if searchInitial:
         authors = [a for a in authors  if a['last_name'].startswith(searchInitial)]
 
-
     return locals()
-
-
