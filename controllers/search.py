8dbe75e9db49fb7604052471eca8688cfdda5a58
Nils Weiher <weiher@ub.uni-heidelberg.de>
2019-07-19 11:27:45 +0200

    Fix error with utf-8 in searchInitial in search/authors, https://gitlab.ub.uni-heidelberg.de/wit/verlag-portale/issues/291

--- controllers/search.py
+++ controllers/search.py
@@ -7,24 +7,26 @@ press = ompdal.getPress(myconf.take('omp.press_id'))
 
 
 def authors():
-    searchInitial = request.vars.searchInitial
+    searchInitial = unicode(request.vars.searchInitial, 'utf-8') if request.vars.searchInitial else None
     authors, author_index = [], set()
     prev_author, prev_index = '',''
     prev = 1
 
-    for i,a in enumerate(ompdal.getAuthorsByPress(press.press_id)):
-        initial = a['last_name'][:1]
+    for a in ompdal.getAuthorsByPress(press.press_id):
+        last_name = a['last_name']
+        first_name = a['first_name']
+        initial = last_name[:1]
         if initial != prev_index:
             a['initial'] = initial.upper()
-        this_author = unicode('{}{}').format(unicode(a['last_name']), unicode(a['first_name']))
+        this_author = unicode('{}{}').format(last_name, first_name)
         prev = prev + 1 if prev_author == this_author else 1
         a['index'] = prev
-        authors.append(a)
+        if searchInitial and last_name.startswith(searchInitial):
+            authors.append(a)
+        elif not searchInitial:
+            authors.append(a)
         author_index.add(initial)
         prev_author = this_author
         prev_index = initial
-
-    if searchInitial:
-        authors = [a for a in authors  if a['last_name'].startswith(searchInitial)]
 
     return locals()
