15af0dc8b4bba725e53548cedd5e7c227870140d
withanage <dulip.withanage@gmail.com>
2020-04-27 07:13:14 +0200

    Announcements

---
+++ controllers/aktuelles.py
@@ -0,0 +1,19 @@
+def eintrag():
+    if request.args and request.args[0] :
+        announcement_id = request.args[0]
+        n = ompdal.getAnnouncementSettings(announcement_id).as_list()
+
+        t = list(filter(lambda e: e['locale'] == locale and e['setting_name'] == 'title', n))
+        title = XML(t[0]['setting_value']) if t else ''
+
+        dl = list(filter(lambda e: e['locale'] == locale and e['setting_name'] == 'description', n))
+        description = XML(dl[0]['setting_value']) if dl else ''
+        return locals()
+
+
+
+
+
+    else:
+        session.flash = T('invalid request')
+        redirect(URL('index'))
